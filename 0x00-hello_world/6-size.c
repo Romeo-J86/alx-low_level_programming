d21b28ec2379d2f05d28ab80c70b2d22278be46a
jerenyamaromeo <jerenyamaromeo@gmail.com>
2022-09-08 20:56:33 +0200

    12-additional-test

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,8 +6,8 @@
  * Return: Always 0 (Success)
  *
  */
- int main(void)
- {
+int main(void)
+{
 char c;
 int i;
 long b;
@@ -20,4 +20,4 @@ printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
 printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(j));
 printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 return (0);
- }
+}
