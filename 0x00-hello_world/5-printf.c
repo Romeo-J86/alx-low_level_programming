f98aac3d07511f7bbfd0d8a3451a569b31ae4482
Romeo-J86 <43685783+Romeo-J86@users.noreply.github.com>
2022-09-08 21:27:18 +0200

    Update 5-printf.c
    pp

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,11 +5,10 @@
  *
  *Return: Always 0 (Success)
  *
- */
+*/
 
 int main(void) 
 {
-printf("with proper grammar, but the outcome is a piece of art,");
-putchar(CR);
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
