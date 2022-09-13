d7e39616daaa3fcbd2f15eb913bb7e62cea7d5b0
jerenyamaromeo <jerenyamaromeo@gmail.com>
2022-09-13 23:37:31 +0200

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - check description
+ * Description - return the absolute value of a number
+ * @n : number to be checked
+ * Return: the absolute value
+ */
+
+int _abs(int n)
+{
+
+return (n >= 0 ? n : n * -1);
+
+}
