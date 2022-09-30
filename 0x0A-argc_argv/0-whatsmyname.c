e86114f3e71f7f1c298c890e1f61cc479c01e2d1
jerenyamaromeo <jerenyamaromeo@gmail.com>
2022-09-30 14:03:14 +0200

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - a program that prints its own name.
+ * @argc: argument count
+ * @argv: argument values
+ *
+ * Return: 0
+ */
+int main(__attribute__((unused)) int argc, char **argv)
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
